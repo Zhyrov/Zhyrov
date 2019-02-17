eda33455f0f66224e4f2fe2c731663f05a5eccf4
Zhyrov <47709075+Zhyrov@users.noreply.github.com>
2019-02-17 10:12:30 +0200

    Add files via upload

---
+++ ZhyrovHomeWork1.java
@@ -0,0 +1,11 @@
+package com.epam.lesson1;
+
+public class ZhyrovHomeWork1 {
+    public static void main(String [] arg){
+        System.out.println("* * * *   * * * *      *      *       *");
+        System.out.println("*         *     *     * *     * *   * *");
+        System.out.println("* *       * * * *    *   *    *   *   *");
+        System.out.println("*         *         * * * *   *       *");
+        System.out.println("* * * *   *        *       *  *       *");
+    }
+}
